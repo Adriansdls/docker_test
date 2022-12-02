1eadcf0a3557d8fa4d8a1485388ae174312a8261
Adriansdls <adrian.s.delasierra@gmail.com>
2022-12-02 19:48:27 +0100

    test
    test

---
+++ app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from sqlalchemy import create_engine
+from datetime import datetime
+from flask_restx import Api, Namespace, Resource, \
+    reqparse, inputs, fields
+
+user = "schedulin"
+passw = "MySQLIsFun"
+host = "35.231.228.133"
+database = "schedulin"
+
+app = Flask(__name__)
+
+def connect():
+    db = create_engine(
+    'mysql+pymysql://{0}:{1}@{2}/{3}' \
+        .format(user, passw, host, database), \
+    connect_args = {'connect_timeout': 10})
+    conn = db.connect()
+    return conn
+
+def disconnect(conn):
+    conn.close()
